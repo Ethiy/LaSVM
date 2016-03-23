f130a21219fc24eef7da32beea79d6ad4fe2cf24
Ethiy <oussama.ennafii@polytechnique.org>
2016-03-23 23:20:40 +0100

    load split file func+la_svm cleaning

---
+++ io.cpp
@@ -0,0 +1,84 @@
+
+#include <cstdlib>
+
+#include <iostream>
+#include <fstream>
+
+#include <boost/algorithm/string.hpp>
+
+#include "io.hpp"
+
+
+using namespace std;
+
+
+vector<ID> split_file_load( char* split_file_name , int& is_binary_file ){
+    vector< ID > splits;
+    int instance_index_in = 0;
+    int labels_in = 0;
+
+    string buffer_line;
+    unsigned long line_num = 0;
+
+    ifstream split_file;
+    split_file.open( split_file_name);
+    string name (split_file_name) ;
+    
+    if( split_file.is_open() ){
+
+        vector<string> strings,strings_ ;
+        getline( split_file , buffer_line );
+        strings.clear();
+        boost::split(strings , buffer_line , boost::is_any_of("\t "));
+        boost::split( strings_ , name , boost::is_any_of("/\\"));
+        strings_.pop_back();
+        strings_.push_back( strings.back() );
+        name = boost::algorithm::join(strings_, "/");
+        split_file_name = const_cast<char*>( name.c_str() );
+        line_num ++;
+
+        getline( split_file , buffer_line );
+        strings.clear();
+        boost::split(strings , buffer_line , boost::is_any_of("\t "));
+        is_binary_file = atoi( (strings.back()).c_str() );
+        line_num ++;
+
+        getline( split_file , buffer_line );
+        strings.clear();
+        boost::split(strings , buffer_line , boost::is_any_of("\t "));
+        instance_index_in =  atoi( (strings.back()).c_str() );
+        line_num ++;
+
+        getline( split_file , buffer_line );
+        strings.clear();
+        boost::split(strings , buffer_line , boost::is_any_of("\t "));
+        labels_in =  atoi( (strings.back()).c_str() );
+        line_num ++;
+
+        cout << "[split file: binary: " << is_binary_file << ", new_indices: " << instance_index_in << ", new_labels:" << labels_in << "]" << endl;
+
+        if( !instance_index_in ) 
+            return splits;
+        int label;
+        long index;
+        while( split_file.peek() != EOF ){
+            cout << "line_num : " << line_num << endl;
+            label =0;
+            index = 0;
+            getline( split_file , buffer_line );
+            strings.clear();
+            boost::split(strings , buffer_line , boost::is_any_of("\t "));
+            index =  atol( strings[0].c_str() );
+            if( label )
+                label = atoi( strings[1].c_str() );
+            splits.push_back(ID( index-1 , label )); // C++ starts from 0
+            line_num ++;
+        }
+        split_file.close();
+    }
+
+    else{
+        cout << "Could not load split file:" << split_file_name << endl; 
+        exit( EXIT_FAILURE );
+    }
+}
---
+++ io.hpp
@@ -0,0 +1,19 @@
+#ifndef IO_H
+#define IO_H
+
+#include <vector>
+
+class ID{ // class to hold split file indices and labels
+public:
+    unsigned long x;
+    int y;
+    ID() : x(0), y(0) {}
+    ID(unsigned long x1,int y1) : x(x1), y(y1) {}
+};
+
+// IDs will be sorted by index, not by label.
+bool operator<(const ID& x, const ID& y){
+    return x.x < y.x;
+}
+
+vector<ID> split_file_load( char* file_name , int& is_binary_file , int& instance_index_in , int& labels_in );
